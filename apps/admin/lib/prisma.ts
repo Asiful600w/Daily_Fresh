7d5b2500e56f39b534e95358e044f60896aaa147
Daily Fresh Admin <admin@dailyfresh.com>
2026-02-03 01:07:31 +0600

    Fix Prisma Build: Remove explicit constructor config (rely on env vars)

--- apps/admin/lib/prisma.ts
+++ apps/admin/lib/prisma.ts
@@ -2,14 +2,7 @@
 import { PrismaClient } from "@prisma/client"
 
 const prismaClientSingleton = () => {
-    return new PrismaClient({
-        // @ts-ignore
-        datasources: {
-            db: {
-                url: process.env.DATABASE_URL,
-            },
-        },
-    })
+    return new PrismaClient()
 }
 
 declare global {
