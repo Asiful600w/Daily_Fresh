7dca1213b2a0ba334de8389110c7c10645ef82e8
Daily Fresh Admin <admin@dailyfresh.com>
2026-02-03 00:05:35 +0600

    Fix Prisma Client type error with @ts-ignore

--- apps/admin/lib/prisma.ts
+++ apps/admin/lib/prisma.ts
@@ -3,11 +3,8 @@ import { PrismaClient } from "@prisma/client"
 
 const prismaClientSingleton = () => {
     return new PrismaClient({
-        datasources: {
-            db: {
-                url: process.env.DATABASE_URL,
-            },
-        },
+        // @ts-ignore
+        datasourceUrl: process.env.DATABASE_URL,
     })
 }
 
